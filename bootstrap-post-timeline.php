c893a0f4985218dccaf2f29381604a8eae69106d
miko <miko@stn.pl>
2016-01-04 11:09:59 +0100

    fix - current yaer changed to last recent post year

--- bootstrap-post-timeline.php
+++ bootstrap-post-timeline.php
@@ -283,7 +283,8 @@ class bootstrapPostTimeline {
         // start from year - current
         $post_from_year = $atts['from_year'];
         if (!$post_from_year) {
-            $post_from_year = date('Y');
+            $recent_posts = wp_get_recent_posts( array('numberposts' => 1, 'post_type' => $this->post_type));
+            $post_from_year = date('Y', strtotime($recent_posts[0]['post_date']));
         }
         $args['year'] = $post_from_year;
         $this->post_from_year = $args['year'];
